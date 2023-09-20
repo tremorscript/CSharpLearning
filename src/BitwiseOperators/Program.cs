0ec07f972c564fbe2f992d1b6e90560f4449255b
tremorScript <tremorscript@gmail.com>
2023-09-20 20:02:32 +0530

    Test Message

---
+++ src/BitwiseOperators/Program.cs
@@ -0,0 +1,46 @@
+﻿
+[Flags]
+internal enum Features
+{
+    None = 0,
+    Brakes = 1,
+    Radio = 2,
+    AirConditioning = 4
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        // a method that converts a number to binary
+        Action<int> b = n => Console.WriteLine(Convert.ToString(n, 2).PadLeft(8, '0'));
+
+        b(1); //output - 00000001
+        b(1 + 1); //output - 00000010
+
+        var v = 0b1010;
+        var x = 0b0110;
+        b(v & x); // 'and' output - 00000010
+        b(v | x); // 'or' output - 00001110
+        b(v ^ x); // 'xor' output - 00001100
+
+        var hasAC = Features.AirConditioning;
+        var hasBrakes = Features.Brakes;
+
+        if (hasAC == Features.AirConditioning && hasBrakes == Features.Brakes)
+        {
+            // Do some work
+        }
+
+        var features = Features.AirConditioning | Features.Brakes;
+        Console.WriteLine(features);
+        b((int)features);
+
+        if ((features & Features.Brakes) == Features.Brakes)
+        {
+            Console.WriteLine("Has Brakes");
+        }
+    }
+}
---
+++ src/ReplaySubject/ReplaySubject/NewsHeadlineFeed.cs
@@ -0,0 +1,76 @@
+﻿
+
+//*********************************************************************************//
+//***                                                                           ***//
+//*** The NewsHeadlineFeed class is just a mock news feed in the form of an     ***//
+//*** observable sequence in Reactive Extensions.                               ***//
+//***                                                                           ***//
+//*********************************************************************************//
+
+using System.Reactive.Concurrency;
+using System.Reactive.Linq;
+
+internal class NewsHeadlineFeed
+{
+    //*** A list of predefined news events to combine with a simple location string ***//
+    private static readonly string[] newsEvents =
+    {
+        "A tornado occurred ",
+        "Weather watch for snow storm issued ",
+        "A robbery occurred ",
+        "We have a lottery winner ",
+        "An earthquake occurred ",
+        "Severe automobile accident "
+    };
+
+    //*** A list of predefined location strings to combine with a news event. ***//
+    private static readonly string[] newsLocations =
+    {
+        "in your area.",
+        "in Dallas, Texas.",
+        "somewhere in Iraq.",
+        "Lincolnton, North Carolina",
+        "Redmond, Washington"
+    };
+    private string feedName; // Feedname used to label the stream
+    private IObservable<string> headlineFeed; //The actual data stream
+    private Random rand = new Random(); // Used to stream random headlines.
+
+    public NewsHeadlineFeed(string name)
+    {
+        feedName = name;
+
+        //*****************************************************************************************//
+        //*** Using the Generate operator to generate a continuous stream of headline that occur ***//
+        //*** randomly within 5 seconds.                                                        ***//
+        //*****************************************************************************************//
+        headlineFeed = Observable.Generate(
+            // the initial string value
+            RandNewsEvent(),
+            // the resulting string is passed here, condition to terminate the loop
+            evt => true,
+            // the iterator or the loop called after the above condition is checked
+            evt => RandNewsEvent(),
+            // the selector function, gives you the opportunity to change the value that the iterator function spat out.
+            evt =>
+            {
+                Thread.Sleep(rand.Next(3000));
+                return evt;
+            },
+            // the scheduler on which this observable is executed.
+            Scheduler.ThreadPool);
+    }
+
+    public IObservable<string> HeadlineFeed { get { return headlineFeed; } }
+
+
+    //****************************************************************//
+    //*** Some very simple formatting of the headline event string ***//
+    //****************************************************************//
+    private string RandNewsEvent()
+    {
+        return "Feedname     :" +
+            $" {feedName}\nHeadline     :" + 
+            $" {newsEvents[rand.Next(newsEvents.Length)]}{newsLocations[rand.Next(newsLocations.Length)]}";
+    }
+}
